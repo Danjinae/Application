304bba97ca8064d11eda3e81422e5a5fc516ecf2
Hojin <hojin180@ajou.ac.kr>
2021-11-09 06:19:37 +0900

    feat: 게시글 작성버튼 클릭시 작성화면 호출

--- app/src/main/java/com/danjinae/FreeBoardFragment.kt
+++ app/src/main/java/com/danjinae/FreeBoardFragment.kt
@@ -1,14 +1,19 @@
 package com.danjinae
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.danjinae.databinding.FragmentFreeBoardBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.tabs.TabLayout
 
 
 class FreeBoardFragment : Fragment() {
+
+    private lateinit var fabwrite: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +27,19 @@ class FreeBoardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_free_board, container, false)
+
+        val view: View = inflater.inflate(R.layout.fragment_free_board, container, false)
+        fabwrite = view.findViewById(R.id.fab_write)
+
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        fabwrite.setOnClickListener{
+            val intent = Intent (this.context, FreeBoardActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
