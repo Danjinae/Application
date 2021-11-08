48f58f27d634408817f9276cc5cf21319fdcf2b1
Hojin <hojin180@ajou.ac.kr>
2021-11-09 04:34:04 +0900

    feat: 게시글작성 Activity 추가및 layout 작성

---
+++ app/src/main/java/com/danjinae/FreeBoardActivity.kt
@@ -0,0 +1,11 @@
+package com.danjinae
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FreeBoardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_free_board)
+    }
+}
