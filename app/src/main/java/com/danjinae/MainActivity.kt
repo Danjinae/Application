dd552d246b22f662f3d7732cf466f6b7bf27a321
Hojin <hojin180@ajou.ac.kr>
2021-11-08 22:23:56 +0900

    BottomNavigation(게시판,차량,홈,관리비,민원) 추가

--- app/src/main/java/com/danjinae/MainActivity.kt
+++ app/src/main/java/com/danjinae/MainActivity.kt
@@ -2,10 +2,42 @@ package com.danjinae
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val frame_l: FrameLayout by lazy {
+        findViewById(R.id.frame_l)
+    }
+
+    private val bn_navi: BottomNavigationView by lazy {
+        findViewById(R.id.bn_navi)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(frame_l.id, HomeFragment()).commit()
+
+        bn_navi.setOnNavigationItemSelectedListener{
+            replaceFragment(
+                when (it.itemId){
+                    R.id.menu_car -> CarFragment()
+                    R.id.menu_community -> CommunityFragment()
+                    R.id.menu_home -> HomeFragment()
+                    R.id.menu_management_fee -> MangementFeeFragment()
+                    else -> ComplaintFragment()
+                }
+
+            )
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(frame_l.id, fragment).commit()
     }
 }
