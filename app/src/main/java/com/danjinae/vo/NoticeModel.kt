01447124c97ca11e353c1ef5a7b80b17aef8e53c
Hojin <hojin180@ajou.ac.kr>
2021-11-11 20:28:29 +0900

    fix: 공지 VO 수정

--- app/src/main/java/com/danjinae/vo/NoticeModel.kt
+++ app/src/main/java/com/danjinae/vo/NoticeModel.kt
@@ -4,4 +4,19 @@ class NoticeModel {
     var catId: Int = 0
     var content: String? = null
     var id: Int = 0
+    var startData: List<Date>? = null
+    var endData: List<Date>? = null
+}
+
+class Date {
+    var date: Int = 0
+    var day: Int = 0
+    var hours: Int = 0
+    var minutes: Int = 0
+    var month: Int = 0
+    var nanos: Int = 0
+    var seconds: Int = 0
+    var time: Int = 0
+    var timezoneOffset: Int = 0
+    var year: Int = 0
 }
