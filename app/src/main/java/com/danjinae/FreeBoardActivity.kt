e9086ad6594af38d82034deabf895553a04204a8
Hojin <hojin180@ajou.ac.kr>
2021-11-18 07:52:55 +0900

    feat: 글작성API 적용

--- app/src/main/java/com/danjinae/FreeBoardActivity.kt
+++ app/src/main/java/com/danjinae/FreeBoardActivity.kt
@@ -1,11 +1,58 @@
 package com.danjinae
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.danjinae.databinding.ActivityFreeBoardBinding
+import com.danjinae.vo.PostModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class FreeBoardActivity : AppCompatActivity() {
+    lateinit var binding: ActivityFreeBoardBinding
+    lateinit var btnPost: Button
+    lateinit var postTitle: EditText
+    lateinit var postContent: EditText
+    val data = arrayListOf<Post>()
+    lateinit var adapter: BoardAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_free_board)
+        binding= ActivityFreeBoardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var postModel = PostModel()
+        postTitle = binding.editPostTitle
+        postContent = binding.editPostContents
+
+        Log.d("등록", postTitle.text.toString())
+
+        binding.btnPost.setOnClickListener {
+            Log.d("등록", "진입")
+            postModel.aptId = 1
+            postModel.content = postContent.text.toString()
+            postModel.title =  postTitle.text.toString()
+            postModel.userId = 4
+
+            val call: Call<PostModel> = RetrofitClient.networkService.addPost(postModel)
+            call.enqueue(object : Callback<PostModel> {
+                override fun onResponse(
+                    call: Call<PostModel>,
+                    response: Response<PostModel>
+                ) {
+                    if (response.isSuccessful) {
+                        Log.d("조회", "성공 : ${response.raw()}")
+                    }
+                    Log.d("조회2", "실패 : ${response.errorBody()?.string()!!}")
+                }
+
+                override fun onFailure(call: Call<PostModel>, t: Throwable) {
+                    Log.d("조회3", "실패 : $t")
+                }
+            })
+        }
+
     }
 }
--- app/src/main/java/com/danjinae/vo/PostModel.kt
+++ app/src/main/java/com/danjinae/vo/PostModel.kt
@@ -1,10 +1,41 @@
 package com.danjinae.vo
 
-class PostModel {
+import android.os.Parcel
+import android.os.Parcelable
 
-    var aptId: Int = 0
-    var postId: Int = 0
+data class PostModel (
+    var aptId: Int = 0,
+    var content: String? = null,
+    var title: String? = null,
     var userId: Int = 0
-    var title: String? = null
-    var content: String? = null
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(aptId)
+        parcel.writeString(content)
+        parcel.writeString(title)
+        parcel.writeInt(userId)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<PostModel> {
+        override fun createFromParcel(parcel: Parcel): PostModel {
+            return PostModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<PostModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
+
