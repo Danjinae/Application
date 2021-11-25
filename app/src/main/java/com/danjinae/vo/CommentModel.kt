f9b6d72760b0c9466aa8905718cb31aa27144403
Hojin <hojin180@ajou.ac.kr>
2021-11-25 13:47:49 +0900

    안쓰는 class 삭제

--- app/src/main/java/com/danjinae/vo/CommentModel.kt
+++
@@ -1,7 +0,0 @@
-package com.danjinae.vo
-
-class CommentModel {
-    val comment: String? = null
-    val userId: Int = 0
-   // val postId: Int = 0
-}
--- app/src/main/java/com/danjinae/vo/ComplaintModel.kt
+++
@@ -1,7 +0,0 @@
-package com.danjinae.vo
-
-class ComplaintModel {
-    var apt: Int = 0
-    var context: String? = null
-    var userId: Int = 0
-}
