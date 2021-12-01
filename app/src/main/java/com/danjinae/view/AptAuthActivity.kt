e7c1ed8f94b5b299c2768cf3f9804d419a58c12d
Hojin <hojin180@ajou.ac.kr>
2021-12-02 07:34:48 +0900

    feat: 입주민 인증 추가

---
+++ app/src/main/java/com/danjinae/view/AptAuthActivity.kt
@@ -0,0 +1,12 @@
+package com.danjinae.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.danjinae.R
+
+class AptAuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apt_auth)
+    }
+}
