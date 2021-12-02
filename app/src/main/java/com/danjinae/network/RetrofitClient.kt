075263fa98e207d56d886852bf099e233792f590
Hojin <hojin180@ajou.ac.kr>
2021-12-02 12:15:03 +0900

    feat: null값 데이터 통신할때 처리 기능 추가

--- app/src/main/java/com/danjinae/network/RetrofitClient.kt
+++ app/src/main/java/com/danjinae/network/RetrofitClient.kt
@@ -2,16 +2,20 @@ package com.danjinae.network
 
 import android.app.Application
 import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.reflect.Type
 import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 
 object RetrofitClient : Application() {
 
     var networkService: RetrofitService
-
+    val URL = "http://101.101.219.69:8080/"
     private val client: OkHttpClient
     init {
     val interceptor = HttpLoggingInterceptor().apply {
@@ -25,7 +29,8 @@ object RetrofitClient : Application() {
     }
     val retrofit: Retrofit
         get() = Retrofit.Builder()
-            .baseUrl("http://101.101.219.69:8080/")
+            .baseUrl(URL)
+            .addConverterFactory(NullOnEmptyConverterFactory())
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
@@ -33,4 +38,20 @@ object RetrofitClient : Application() {
     init {
         networkService = retrofit.create(RetrofitService::class.java)
     }
+
+    class NullOnEmptyConverterFactory @Inject constructor() : Converter.Factory() {
+
+        override fun responseBodyConverter(
+            type: Type,
+            annotations: Array<out Annotation>,
+            retrofit: Retrofit
+        ) = Converter<ResponseBody, Any?> {
+            if (it.contentLength() != 0L) retrofit.nextResponseBodyConverter<Any?>(
+                this,
+                type,
+                annotations
+            ).convert(it) else null
+        }
+    }
 }
+
