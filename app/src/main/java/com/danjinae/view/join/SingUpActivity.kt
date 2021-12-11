44a07115534136786c566d8d85218c1765eb30c2
Hojin <hojin180@ajou.ac.kr>
2021-12-11 18:31:10 +0900

    fix: 회원가입 유효성 검사

--- app/src/main/java/com/danjinae/view/join/SingUpActivity.kt
+++ app/src/main/java/com/danjinae/view/join/SingUpActivity.kt
@@ -38,31 +38,57 @@ class SingUpActivity : AppCompatActivity() {
         binding.btnJoin.setOnClickListener {
             singUp.password = binding.editRePass.text.toString()
             singUp.phone = phoneNum
-            if(!Pattern.matches("^[a-zA-Z][0-9]{8,16}$",binding.editRePass.text.toString()))
-            {
-                Log.d("d","a")
-                Toast.makeText(this,"비밀번호형식을 지켜주세요.", Toast.LENGTH_SHORT).show()
-            }else{
-                val call: Call<Boolean> = RetrofitClient.networkService.putSingUp(singUp)
-                call.enqueue(object : Callback<Boolean> {
-                    override fun onResponse(
-                        call: Call<Boolean>,
-                        response: Response<Boolean>
-                    ){
-                        if(response.isSuccessful){
-                            Log.d("조회", "성공 : ${response.raw()}")
-                            binding.btnConfirm.isEnabled = true
-                        }else{
+            val call: Call<Boolean> = RetrofitClient.networkService.putSingUp(singUp)
+            call.enqueue(object : Callback<Boolean> {
+                override fun onResponse(
+                    call: Call<Boolean>,
+                    response: Response<Boolean>
+                ){
+                    if(response.isSuccessful){
+                        Log.d("조회", "성공 : ${response.raw()}")
+                        binding.btnConfirm.isEnabled = true
+                    }else{
 
-                        }
                     }
-                    override fun onFailure(call: Call<Boolean>, t: Throwable) {
-                        Log.d("연결", "실패 : $t")
-                    }
-                })
-                Toast.makeText(this,"회원가입이 되었습니다.", Toast.LENGTH_SHORT).show()
-            }
+                }
+                override fun onFailure(call: Call<Boolean>, t: Throwable) {
+                    Log.d("연결", "실패 : $t")
+                }
+            })
+            Toast.makeText(this,"회원가입이 되었습니다.", Toast.LENGTH_SHORT).show()
         }
+
+        binding.editPass.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                binding.password.isVisible =
+                    !Pattern.matches("^(?=.*[A-Za-z])(?=.*[0-9]).{8,16}$",binding.editPass.text.toString())
+
+                if (binding.editPass.text.toString()==binding.editRePass.text.toString()) {
+                    binding.btnJoin.isEnabled = true
+                    binding.passSame.isVisible = false
+                }else{
+                    binding.passSame.isVisible = true
+                    binding.btnJoin.isEnabled = false
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.password.isVisible =
+                    !Pattern.matches("^(?=.*[A-Za-z])(?=.*[0-9]).{8,16}\$",binding.editPass.text.toString())
+
+                if (binding.editPass.text.toString().equals(binding.editRePass.text.toString())) {
+                    binding.btnJoin.isEnabled = true
+                    binding.passSame.isVisible = false
+                }else{
+                    binding.passSame.isVisible = true
+                    binding.btnJoin.isEnabled = false
+                }
+            }
+        })
 
         binding.editRePass.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
